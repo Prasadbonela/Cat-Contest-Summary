4e1bc9713435324439dca8edc40ea1cda6f45447
Sofia Krylova <sofia_krylova@epam.com>
2022-06-17 22:16:18 +0200

    Initial

---
+++ src/main/java/com/epam/autotasks/CatContestHelper.java
@@ -0,0 +1,20 @@
+package com.epam.autotasks;
+
+import java.util.List;
+
+public class CatContestHelper {
+
+    public static final Integer CARRIER_THRESHOLD = 30;
+
+    public Integer getCarrierNumber(List<Cat> cats) {
+        return null;
+    }
+
+    public String getCarrierId(List<Cat> cats) {
+        return null;
+    }
+
+    public Integer countTeamAwards(List<Cat> cats) {
+        return null;
+    }
+}
